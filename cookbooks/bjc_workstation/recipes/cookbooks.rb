4fa6dc591e7021b6de24e279f935b86861a1063e
Sean Carolan <scarolan@gmail.com>
2017-03-26 01:39:16 +0000

    Removes pre-configured bjc-ecommerce, which has hard coded settings for the demo

--- cookbooks/bjc_workstation/recipes/cookbooks.rb
+++ cookbooks/bjc_workstation/recipes/cookbooks.rb
@@ -18,26 +18,26 @@ directory "#{home}/cookbooks" do
   action :create
 end
 
-node['bjc_workstation']['cookbooks'].each do |cb|
-  execute "Copy cookbooks into home directory" do
-    action :run
-    command "xcopy /E /I /Q /Y \"#{Chef::Config[:file_cache_path]}\\bjc\\cookbooks\\#{cb}\" \"#{home}\\cookbooks\\#{cb}\""
-  end
-end
-
-template "#{home}/cookbooks/bjc-ecommerce/.kitchen.yml" do
-  action :create
-  source 'kitchen_ecom.yml.erb'
-end
-
+# node['bjc_workstation']['cookbooks'].each do |cb|
+#   execute "Copy cookbooks into home directory" do
+#     action :run
+#     command "xcopy /E /I /Q /Y \"#{Chef::Config[:file_cache_path]}\\bjc\\cookbooks\\#{cb}\" \"#{home}\\cookbooks\\#{cb}\""
+#   end
+# end
+# 
+# template "#{home}/cookbooks/bjc-ecommerce/.kitchen.yml" do
+#   action :create
+#   source 'kitchen_ecom.yml.erb'
+# end
+# 
 # This puts a minimal working git config and commit history in place
-cookbook_file "#{home}/git_dir.zip" do
-  action :create
-  source 'git_dir.zip'
-end
-
-windows_zipfile "#{home}/cookbooks/bjc-ecommerce" do
-  action :unzip
-  overwrite true
-  source "#{home}/git_dir.zip"
-end
+# cookbook_file "#{home}/git_dir.zip" do
+#   action :create
+#   source 'git_dir.zip'
+# end
+# 
+# windows_zipfile "#{home}/cookbooks/bjc-ecommerce" do
+#   action :unzip
+#   overwrite true
+#   source "#{home}/git_dir.zip"
+# end
